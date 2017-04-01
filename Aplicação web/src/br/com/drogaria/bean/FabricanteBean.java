49ddf6a9fcd9e91a09b0f1ce37c2251c5f920266
jgabr <jgabrielsantanadasilva@outlook.com>
2017-04-01 14:18:47 -0300

    Commit 15 - Capturando dados da tela

--- Aplicação web/src/br/com/drogaria/bean/FabricanteBean.java
+++ Aplicação web/src/br/com/drogaria/bean/FabricanteBean.java
@@ -3,14 +3,28 @@ package br.com.drogaria.bean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import br.com.drogaria.domain.Fabricante;
 import br.com.drogaria.util.FacesUtil;
-
 
 @ManagedBean
 @ViewScoped
 public class FabricanteBean {
+	private Fabricante fabricanteCadastro;
 
-	public void salvar(){
+	public Fabricante getFabricanteCadastro() {
+		if (fabricanteCadastro == null) {
+			fabricanteCadastro = new Fabricante();
+		}
+		return fabricanteCadastro;
+	}
+
+	public void setFabricanteCadastro(Fabricante fabricanteCadastro) {
+		this.fabricanteCadastro = fabricanteCadastro;
+	}
+
+	public void salvar() {
+		FacesUtil.adicionarMsgInfo(fabricanteCadastro.toString());
+
 		FacesUtil.adicionarMsgInfo("Fabricante salvo com sucessso");
 	}
 }
--- Aplicação web/src/br/com/drogaria/bean/FuncionarioBean.java
+++ Aplicação web/src/br/com/drogaria/bean/FuncionarioBean.java
@@ -3,13 +3,28 @@ package br.com.drogaria.bean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import br.com.drogaria.domain.Funcionario;
 import br.com.drogaria.util.FacesUtil;
 
 @ManagedBean
 @ViewScoped
 public class FuncionarioBean {
+	private Funcionario funcionarioCadastro;
+
+	public Funcionario getFuncionarioCadastro() {
+		if (funcionarioCadastro == null) {
+			funcionarioCadastro = new Funcionario();
+		}
+		return funcionarioCadastro;
+	}
+
+	public void setFuncionarioCadastro(Funcionario funcionarioCadastro) {
+		this.funcionarioCadastro = funcionarioCadastro;
+	}
 
 	public void salvar() {
+		FacesUtil.adicionarMsgInfo(funcionarioCadastro.toString());
+		
 		FacesUtil.adicionarMsgInfo("Funcionário salvo com sucessso");
 	}
 }
