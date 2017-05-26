cc9db61aa60a570ed5499b4129c3e15b532c399e
jgabr <jgabrielsantanadasilva@outlook.com>
2017-05-26 15:17:30 -0300

    Commit 27 - Implementação de Autenticação de usuário

---
+++ Aplicação web/src/br/com/sysbebidas/bean/AutenticacaoBean.java
@@ -0,0 +1,51 @@
+package br.com.sysbebidas.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import br.com.sysbebidas.dao.FuncionarioDAO;
+import br.com.sysbebidas.domain.Funcionario;
+import br.com.sysbebidas.util.FacesUtil;
+
+@ManagedBean
+@SessionScoped
+public class AutenticacaoBean {
+	private Funcionario funcionarioLogado;
+	
+	public Funcionario getFuncionarioLogado() {
+		if(funcionarioLogado ==  null){
+			funcionarioLogado = new Funcionario();
+		}
+		return funcionarioLogado;
+	}
+	
+	public void setFuncionarioLogado(Funcionario funcionarioLogado) {
+		this.funcionarioLogado = funcionarioLogado;
+	}
+	
+	public String autenticar(){
+		try{
+			FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
+			funcionarioLogado = funcionarioDAO.autenticar(funcionarioLogado.getCpf(), DigestUtils.md5Hex(funcionarioLogado.getSenha()));
+			
+			if(funcionarioLogado == null){
+				FacesUtil.adicionarMsgErro("CPF e/ou Senha inválidos");
+				return null;
+			} else{
+				FacesUtil.adicionarMsgInfo("Funcionário autenticado com sucesso");
+				return "/pages/principal.xhtml?faces-redirect=true";
+			}
+		} catch (RuntimeException ex){
+			FacesUtil.adicionarMsgErro("Erro ao tentar autenticar no sistema:" 
+			+ ex.getMessage());
+			return null;
+		}
+	}
+	
+	public String sair (){
+		funcionarioLogado = null;
+		return "/pages/autenticacao.xhtml?faces-redirect=true";
+	}
+}
