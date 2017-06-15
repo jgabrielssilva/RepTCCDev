6fc363b95ac92f522cce494890bffa42afa63c4c
jgabr <jgabrielsantanadasilva@outlook.com>
2017-06-15 10:03:46 -0300

    Commit 30 - Contexto

--- Aplicação web/src/br/com/sysbebidas/util/ContextListener.java
+++ Aplicação web/src/br/com/sysbebidas/util/ContextListener.java
@@ -6,13 +6,12 @@ import javax.servlet.ServletContextListener;
 public class ContextListener implements ServletContextListener {
 
 	@Override
-	public void contextDestroyed(ServletContextEvent sce){
+	public void contextDestroyed(ServletContextEvent sce) {
 		HibernateUtil.getSessionFactory().close();
 	}
-	
+
 	@Override
-	public void contextInitialized(ServletContextEvent sce){
+	public void contextInitialized(ServletContextEvent sce) {
 		HibernateUtil.getSessionFactory().openSession();
 	}
-
 }
